8cb8ac0e2f00ba83783c86ce378a5277233d57ec
Brandon DeRosier <bdero@mit.edu>
2014-12-07 00:00:40 -0500

    Filter out tweets containing URLs and loop forever

--- jaden.py
+++ jaden.py
@@ -1,10 +1,17 @@
 #!/bin/env python2
 
+import pyttsx
 import yaml
-from twitter import Twitter, OAuth
+
 from random import randint
+from time import sleep
+from twitter import Twitter, OAuth
 
 config = yaml.load(open('config.yml'))
+
+
+def get_tts():
+    return pyttsx.init()
 
 
 def get_client():
@@ -17,17 +24,38 @@ def get_client():
 
 
 def get_tweets(screen_name):
-    return twitter.statuses.user_timeline(
+    raw_tweets = twitter.statuses.user_timeline(
         screen_name=screen_name,
         count=200,
         exclude_replies=True,
         include_rts=False
     )
+    print 'Fetched', len(raw_tweets), 'tweets'
+
+    tweets = filter(
+        lambda t: t['text'].find('http') == -1,
+        raw_tweets
+    )
+    print 'Filtered', len(raw_tweets) - len(tweets), 'tweets'
+
+    return tweets
+
+
+def say(tts, message):
+    tts.say(message)
+    tts.runAndWait()
 
 
 if __name__ == '__main__':
     twitter = get_client()
     tweets = get_tweets('officialjaden')
 
-    pick = randint(0, len(tweets) - 1)
-    print tweets[pick]['text']
+    while True:
+        pick = randint(0, len(tweets) - 1)
+        tweet = tweets[pick]['text']
+
+        print tweet
+        tts = get_tts()
+        say(tts, tweet)
+
+        sleep(10)
